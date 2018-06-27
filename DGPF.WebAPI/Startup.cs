547e345e7d6033edb051fd4ecc7cc1c175aaf786
zhangfan6385 <zfcrocodile@sina.com>
2018-06-27 15:05:13 +0800

    startup

--- DGPF.WebAPI/Startup.cs
+++ DGPF.WebAPI/Startup.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.AspNetCore.Cors;
 
 
 namespace DGPF.WebAPI
