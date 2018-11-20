9d8e13dbafde156c0dca5dee3143c19d4671b156
zhangshe <1312719913@qq.com>
2018-11-20 11:08:09 +0800

    修改导入模板及usertype取值

--- UIDP.WebAPI/Controllers/UserController.cs
+++ UIDP.WebAPI/Controllers/UserController.cs
@@ -265,7 +265,7 @@ namespace UIDP.WebAPI.Controllers
                     int _userSex = Convert.ToInt32(dt.Rows[0]["USER_SEX"].ToString());
                     string _deptCode = dt.Rows[0]["ORG_CODE"] == null ? "" : dt.Rows[0]["ORG_CODE"].ToString();
                     string _deptName = dt.Rows[0]["ORG_SHORT_NAME"] == null ? "" : dt.Rows[0]["ORG_SHORT_NAME"].ToString();
-                    string _userType= dt.Rows[0]["AUTHENTICATION_TYPE"] == null ? "" : dt.Rows[0]["AUTHENTICATION_TYPE"].ToString();
+                    string _userType= dt.Rows[0]["USER_TYPE"] == null ? "" : dt.Rows[0]["USER_TYPE"].ToString();
                     UIDP.LOG.SysLog log = new LOG.SysLog();
                     //log.Info(DateTime.Now, d["userId"].ToString(), _name, ClientIp, 0, "info", "",1);
                     log.Info(DateTime.Now, userID, _name, ClientIp, 0, "info", "", 1);
